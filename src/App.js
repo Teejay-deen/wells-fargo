f7c9dd90c63173fe56c15e8792287bc0c87f64fc
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-03-18 12:55:56 +0100

    adding some section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Banner from './components/Banner';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Hero2 from './components/Hero2';
 
 function App() {
   return (
-   <div >
+   <div>
     <Header />
+    <Hero />
+    <Hero2 />
+    <Banner />
    </div>
   );
 }
